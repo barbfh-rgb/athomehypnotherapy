838d5558e15d9348819c3a28d576c20e3e538bcd
barbfh-rgb <barbfh@gmail.com>
2026-04-14 15:24:46 +0100

    Add files via upload

---
+++ glp1-sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'glp1-nutrition-v1';
+const FILES = ['/glp1-nutrition.html', '/glp1-manifest.json', '/glp1-icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
